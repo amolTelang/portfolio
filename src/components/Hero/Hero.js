435fc454638b6c91f3b5ca401d58cf031071b4c6
amolTelang <telangamol0909@gmail.com>
2021-08-14 23:17:43 +0530

    added many things

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section>
+    <LeftSection>
+      <SectionTitle main center>
+      Welcome to <br />
+      My personal portfolio
+      </SectionTitle>
+      <SectionText>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+      </SectionText>
+      <Button onClick={()=>window.location='https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
